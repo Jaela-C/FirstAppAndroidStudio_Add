374a1841e4aa23b452a0c2f2d261fbf45416c8b3
Jaela-C <jaelacarlosama3c@hotmail.com>
2021-08-17 21:57:23 -0500

    Suma de dos números

---
+++ app/src/main/java/com/example/apptest/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.apptest;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et1, et2;
+    private TextView tv3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1 = (EditText) findViewById(R.id.et1);
+        et2 = (EditText) findViewById(R.id.et2);
+        tv3 = (TextView) findViewById(R.id.tv3);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_game, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void sumar(View view){
+        String valor1 = et1.getText().toString();
+        String valor2 = et2.getText().toString();
+        int nro1 = Integer.parseInt(valor1);
+        int nro2 = Integer.parseInt(valor2);
+        int suma = nro1 + nro2;
+        String resultado = String.valueOf(suma);
+        tv3.setText(resultado);
+    }
+}
